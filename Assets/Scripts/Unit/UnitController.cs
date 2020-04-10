8ed5dd7564ed9e90652eba58b1f3b4c58f73180e
Vladyslav <alksgest@gmail.com>
2020-04-10 15:14:11 +0300

    added rotating for unit

--- Assets/Scripts/Unit/UnitController.cs
+++ Assets/Scripts/Unit/UnitController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 using StrategyGame.Assets.Scripts.Building;
+using System;
 
 namespace StrategyGame.Assets.Scripts.Unit
 {
@@ -22,34 +23,55 @@ namespace StrategyGame.Assets.Scripts.Unit
         public GameObject ObjectAttachedTo { get; set; }
 
         private bool _canMove = false;
+        private bool _isRotating = true;
 
         private void FixedUpdate()
         {
             if (_isMoving) Move();
+            if (_isRotating) Rotate();
         }
 
         public void Select()
         {
             Selected = !Selected;
-
-            var renderer = GetComponent<MeshRenderer>();
+            var renderer = GetComponentInChildren<MeshRenderer>();
             renderer.material = Selected ? _selectedMaterial : _defaultMaterial;
         }
 
         public void AskToMove(Vector3 point)
         {
-            Debug.Log(point);
+            // Debug.Log(point);
+            // Debug.Log(this.transform.position);
+
             _pointToMove = point;
 
             _pointToMove.y = this.transform.position.y;
 
             _isMoving = true;
+            _isRotating = true;
 
             if (this.tag == "AttachedToMineUnit")
             {
                 if (ObjectAttachedTo != null)
                 {
                     ObjectAttachedTo.GetComponent<MineEdgeController>().DeatachUnit();
+                }
+            }
+        }
+
+        private void Rotate()
+        {
+            if (_pointToMove != null)
+            {
+                var targetRotation = Quaternion.LookRotation(_pointToMove - transform.position);
+                var angles = targetRotation.eulerAngles;
+                angles.y += 180;
+                targetRotation.eulerAngles = angles;
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10 * Time.deltaTime);
+
+                if(Mathf.Abs(targetRotation.eulerAngles.y - transform.rotation.eulerAngles.y) <= 2)
+                {
+                    _isRotating = false;
                 }
             }
         }
@@ -85,3 +107,71 @@ namespace StrategyGame.Assets.Scripts.Unit
         }
     }
 }
+
+//  private void __RotateToPoint(Vector3 point)
+//         {
+//             var curentPosition = this.transform.position;
+
+//             int direction = GetAngleBase(point, curentPosition);
+
+//             var distance = Mathf.Sqrt(Mathf.Pow(point.x - curentPosition.x, 2) + Mathf.Pow(point.z - curentPosition.z, 2));
+//             var angle = Mathf.Acos(Mathf.Abs(curentPosition.z - point.z) / distance) * 180;
+
+//             // var angle = 0;
+//             Debug.Log("Angle : " + angle);
+//             Debug.Log("Direction : " + direction);
+
+//             switch (direction)
+//             {
+//                 case 0:
+//                 case 180:
+//                     angle = angle + 0;
+//                     break;
+//                 case 90:
+//                     angle = 90 + (90 - angle);
+//                     break;
+//                 case 270:
+//                     angle = 270 + (90 - angle);
+//                     break;
+//             }
+
+//             this.transform.eulerAngles = new Vector3(0, angle, 0);
+
+//             Debug.Log("Angle of rotation after : " + angle);
+//         }
+
+//         private float GetAngleToRotate(Vector3 pointA, Vector3 pointB)
+//         {
+//             Vector3 pointC = new Vector3(pointB.x, 0, pointA.z);
+
+//             Vector3 vectorAB = new Vector3(pointB.x - pointA.x, 0, pointB.z - pointA.z);
+//             Vector3 vectorAC = new Vector3(pointC.x - pointA.x, 0, pointC.z - pointA.z);
+
+//             Console.WriteLine(vectorAB);
+//             Console.WriteLine(vectorAC);
+
+//             var scalar = vectorAB.x * vectorAC.x + vectorAB.z * vectorAC.z;
+
+//             var absVectorAB = Mathf.Sqrt(vectorAB.x * vectorAB.x + vectorAB.z * vectorAB.z);
+//             var absVectorAC = Mathf.Sqrt(vectorAC.x * vectorAC.x + vectorAC.z * vectorAC.z);
+
+//             var cos = scalar / (absVectorAB * absVectorAC);
+//             var angle = Mathf.Acos(cos);
+
+
+//             return angle;
+//         }
+
+//         private int GetAngleBase(Vector3 pointA, Vector3 pointB)
+//         {
+//             var z1 = pointB.z;
+//             var z2 = pointA.z;
+//             var x1 = pointB.x;
+//             var x2 = pointA.x;
+
+//             var angleBase = z1 > z2 && x1 > x2 ? 0 :
+//                             z1 < z2 && x1 > x2 ? 90 :
+//                             z1 < z2 && x1 < x2 ? 180 : 270;
+
+//             return angleBase;
+//         }
